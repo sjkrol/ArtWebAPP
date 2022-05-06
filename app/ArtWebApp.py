91a3b9f0f82a764fc1d8be8599a3851ddca7db00
sjkrol <stephenkrol1997@gmail.com>
2022-05-06 13:39:27 +1000

    Initial Commit

---
+++ app/ArtWebApp.py
@@ -0,0 +1,136 @@
+import json
+import random
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, url_for, session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "8w57ghz3TGgIkL_cdqD5BTRt69w"
+
+paintings = ["a-cow-in-a-landscape", 
+             "a-saddled-race-horse-tied-to-a-fence-1828",
+             "herdsman-with-resting-cattle",
+             "hip-hip-hurrah-1888",
+             "james-abercromby-of-tullibody-esq-1779",
+             "paris-a-rainy-day-1877",
+             "tennis-triptych-centre-panel-1930"]
+
+#load art data
+with open("static/json/art-info.json") as f:
+    art_data = json.load(f)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/experiment")
+def experiment():
+    return render_template("experiment.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/gallery/painting/<string:image_name>", methods=["GET"])
+def painting_data(image_name):
+    
+    image_name = image_name[1:len(image_name)-5]
+    video_name = "/videos/" + image_name + ".mp4"
+    session["video_name"] = video_name
+    session["painting_name"] = image_name
+
+    return redirect(url_for("painting"))
+
+@app.route("/gallery/painting")
+def painting():
+
+    video_name = session["video_name"]
+    painting_data = art_data[session["painting_name"]]
+
+    return render_template("painting.html", 
+                            video_name=video_name,
+                            title=painting_data["Title"],
+                            artist=painting_data["Artist"],
+                            year=painting_data["Year"],
+                            artistpic=painting_data["ArtistPic"],
+                            artistDesc=painting_data["ArtistDesc"],
+                            artistbirthdeath=painting_data["ArtistBirthDeath"],
+                            paintingdesc=painting_data["PaintingDesc"]
+                            )
+
+@app.route("/experiment/consent")
+def consent():
+
+    return render_template("consent.html")
+
+@app.route("/experiment/demographic", methods=['GET', 'POST'])
+def demographic():
+
+    if request.method == 'GET':
+        # assign user ID
+        participant_id = now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        session["participant_id"] = participant_id
+        session["data"] = {
+            "demographic" : {"id" : participant_id, "gender" : None, "age" : None, "lowvision" : None},
+            "responses" : []
+            }
+
+        return render_template("demographic.html")
+    
+    if request.method == 'POST':
+
+        demographic = json.loads(request.data)
+        session["data"]["demographic"]["age"] = demographic['age']
+        session["data"]["demographic"]["lowvision"] = demographic['lowvision']
+        if demographic['gender'] == "Other":
+            session["data"]["demographic"]["gender"] = demographic['other']
+        else:
+            session["data"]["demographic"]["gender"] = demographic['gender']
+        
+        # set painting index
+        session["painting_n"] = 0
+        random.shuffle(paintings)
+
+        return 'success', 200
+
+@app.route('/experiment/questions')
+def questions():
+    
+    if request.method == 'GET':
+        print(session["painting_n"])
+        painting_name = paintings[session["painting_n"]]
+        painting_data = art_data[painting_name]
+        video_name = "/videos/" + painting_name + ".mp4"
+
+        template = render_template("questions.html",
+                               video_name=video_name,
+                               title=painting_data["Title"],
+                               artist=painting_data["Artist"],
+                               year=painting_data["Year"],
+                               paintingdesc=painting_data["PaintingDesc"],
+                               painting_n=session["painting_n"])
+
+        session["painting_n"] += 1
+
+        return template
+
+@app.route('/experiment/questions/back', methods=["POST"])
+def experiment_back():
+
+    if request.method == "POST":
+
+        painting_n = int(session["painting_n"])
+        painting_n -= 1
+        print(painting_n)
+        session["painting_n"] = painting_n
+    
+    return 'success', 200
+
---
+++ app/static/js/utils.js
@@ -0,0 +1,86 @@
+function openImg(id){
+  var image = document.getElementById(id);
+  var source = image.src;
+  const image_name = source.substring(source.lastIndexOf('/') + 1);
+
+  console.log("image_name");
+
+  const request = new XMLHttpRequest();
+  request.open('GET', `/gallery/painting/${JSON.stringify(image_name)}`);
+
+  request.onreadystatechange = function() {
+      if(request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+        window.location.href = "/gallery/painting";
+      }
+  }
+
+  request.send();
+
+}
+
+function returnToGallery(){
+  window.location.href = "/gallery";
+}
+
+function beginExperiment(){
+  window.location.href = "/experiment/consent";
+}
+
+function goToDemographic(){
+
+  c1 = document.getElementById("C1").checked;
+  c2 = document.getElementById("C2").checked;
+  c3 = document.getElementById("C3").checked;
+  c4 = document.getElementById("C4").checked;
+  c5 = document.getElementById("C5").checked;
+  c6 = document.getElementById("C6").checked;
+
+  if(c1 && c2 && c3 && c4 && c5 && c6){
+    window.location.href = "/experiment/demographic";
+  } else{
+    $('#myModal').modal('show');
+    console.log("stop right there criminal scum");
+  }
+
+}
+
+function returnToExplanatory(){
+  window.location.href = "/experiment";
+}
+
+function returnToConsent(){
+  window.location.href = "/experiment/consent";
+}
+
+function sendDemographicInfo(){
+  gender = document.getElementById("gender").value;
+  other = document.getElementById("other_gender").value;
+  age = document.getElementById("age").value;
+  lowvision = document.getElementById("lowvision").value;
+
+  if(gender != "Other" || (gender == "Other" && other != '')){
+
+      console.log("Move along, no issues here")
+      const request = new XMLHttpRequest();
+      // sender = JSON.stringify([gender, other, age, lowvision]);
+      sender = JSON.stringify({"gender" : gender, "other" : other, "age" : age, "lowvision" : lowvision});
+      request.open('POST', '/experiment/demographic');
+
+      request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+          window.location.href = "/experiment/questions";
+        }
+    }
+
+    request.send(sender);
+
+  } else {
+    $('#demographic_modal').modal('show');
+    console.log("Must specify other gender");
+  }
+
+}
+
+function save_responses(){
+    window.location.href = "/experiment/questions"
+}
